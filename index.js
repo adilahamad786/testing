18a6a56bcc508df4b5b5c3638eba9d28a964ca4f
adilahamad786 <adilahamad.personally@gmail.com>
2023-05-04 22:37:44 +0530

    Another way to get promise result using async function.

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const myPromise = new Promise((resolve, reject) => {
     if (flag)
         resolve("Promise has resolved!");
     else
-        reject("Promise has rejected!")
+        reject("Promise has rejected!");
 });
+
+async function fun () {
+    try {
+        let result = await myPromise;
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fun();
 
 myPromise
     .then(result => console.log(result))
