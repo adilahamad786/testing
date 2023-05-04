3d956cc7810ccdc18b92d0c2568984288c80d494
adilahamad786 <adilahamad.personally@gmail.com>
2023-05-04 22:54:20 +0530

    Example of Promise.allSettled!

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// let flag = true;
+let flag = true;
 
-// const myPromise1 = new Promise((resolve, reject) => {
-//     if (flag)
-//         resolve("Promise has resolved!");
-//     else
-//         reject("Promise has rejected!");
-// });
+const myPromise1 = new Promise((resolve, reject) => {
+    if (flag)
+        resolve("Promise has resolved!");
+    else
+        reject("Promise has rejected!");
+});
 
-// const myPromise2 = new Promise((resolve, reject) => {
-//     if (flag)
-//         resolve("Promise has resolved!");
-//     else
-//         reject("Promise has rejected!");
-// });
+const myPromise2 = new Promise((resolve, reject) => {
+    if (flag)
+        resolve("Promise has resolved!");
+    else
+        reject("Promise has rejected!");
+});
 
-// const myPromise3 = new Promise((resolve, reject) => {
-//     if (false)
-//         resolve("Promise has resolved!");
-//     else
-//         reject("Promise has rejected!");
-// });
+const myPromise3 = new Promise((resolve, reject) => {
+    if (false)
+        resolve("Promise has resolved!");
+    else
+        reject("Promise has rejected!");
+});
 
 // // async function fun () {
 // //     try {
@@ -41,12 +41,23 @@
 //     .then(res => console.log(res))
 //     .catch(err => console.log(err));
 
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
-});
+// const promise1 = Promise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, 'foo');
+// });
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-});
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//   console.log(values);
+// });
+
+let result = Promise.allSettled([myPromise1, myPromise2, myPromise3]);
+result
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+
+// Output : [
+//     { status: 'fulfilled', value: 'Promise has resolved!' },
+//     { status: 'fulfilled', value: 'Promise has resolved!' },
+//     { status: 'rejected', reason: 'Promise has rejected!' }
+// ]
