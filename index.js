3875f2f9a8b5cd949083fad71308a5a34ab26498
adilahamad786 <adilahamad.personally@gmail.com>
2023-05-04 22:47:00 +0530

    Best example of Promise.all!

--- index.js
+++ index.js
@@ -1,42 +1,52 @@
-let flag = true;
+// let flag = true;
 
-const myPromise1 = new Promise((resolve, reject) => {
-    if (flag)
-        resolve("Promise has resolved!");
-    else
-        reject("Promise has rejected!");
+// const myPromise1 = new Promise((resolve, reject) => {
+//     if (flag)
+//         resolve("Promise has resolved!");
+//     else
+//         reject("Promise has rejected!");
+// });
+
+// const myPromise2 = new Promise((resolve, reject) => {
+//     if (flag)
+//         resolve("Promise has resolved!");
+//     else
+//         reject("Promise has rejected!");
+// });
+
+// const myPromise3 = new Promise((resolve, reject) => {
+//     if (false)
+//         resolve("Promise has resolved!");
+//     else
+//         reject("Promise has rejected!");
+// });
+
+// // async function fun () {
+// //     try {
+// //         let result = await myPromise;
+// //         console.log(result);
+// //     } catch (error) {
+// //         console.log(error);
+// //     }
+// // }
+
+// // fun();
+
+// // myPromise
+// //     .then(result => console.log(result))
+// //     .catch(error => console.log(error))
+
+// let result = Promise.all([myPromise1, myPromise2, myPromise3]);
+// result
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err));
+
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 100, 'foo');
 });
 
-const myPromise2 = new Promise((resolve, reject) => {
-    if (flag)
-        resolve("Promise has resolved!");
-    else
-        reject("Promise has rejected!");
+Promise.all([promise1, promise2, promise3]).then((values) => {
+  console.log(values);
 });
-
-const myPromise3 = new Promise((resolve, reject) => {
-    if (false)
-        resolve("Promise has resolved!");
-    else
-        reject("Promise has rejected!");
-});
-
-// async function fun () {
-//     try {
-//         let result = await myPromise;
-//         console.log(result);
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// fun();
-
-// myPromise
-//     .then(result => console.log(result))
-//     .catch(error => console.log(error))
-
-let result = Promise.all([myPromise1, myPromise2, myPromise3]);
-result
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
