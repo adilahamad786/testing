4cdbb895c5fa89db8fce0ed93d0f16758103809c
adilahamad786 <adilahamad.personally@gmail.com>
2023-05-04 22:44:59 +0530

    Example of Promise.all!

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 let flag = true;
 
-const myPromise = new Promise((resolve, reject) => {
+const myPromise1 = new Promise((resolve, reject) => {
     if (flag)
         resolve("Promise has resolved!");
     else
         reject("Promise has rejected!");
 });
 
-async function fun () {
-    try {
-        let result = await myPromise;
-        console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
-}
+const myPromise2 = new Promise((resolve, reject) => {
+    if (flag)
+        resolve("Promise has resolved!");
+    else
+        reject("Promise has rejected!");
+});
 
-fun();
+const myPromise3 = new Promise((resolve, reject) => {
+    if (false)
+        resolve("Promise has resolved!");
+    else
+        reject("Promise has rejected!");
+});
 
-myPromise
-    .then(result => console.log(result))
-    .catch(error => console.log(error))
+// async function fun () {
+//     try {
+//         let result = await myPromise;
+//         console.log(result);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// fun();
+
+// myPromise
+//     .then(result => console.log(result))
+//     .catch(error => console.log(error))
+
+let result = Promise.all([myPromise1, myPromise2, myPromise3]);
+result
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
